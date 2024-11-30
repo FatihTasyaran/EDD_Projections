9468f891435f571dba73b0ce5ebb4bbf51614f3d
Fatih Tasyaran <fatihtasyaran@sabanciuniv.edu>
2024-11-30 20:29:45 +0100

    Task finder now also have finely aggregated data

--- task_finder.py
+++ task_finder.py
@@ -6,7 +6,15 @@ api_index = {}
 api_index_counter = 0
 gpu_index = {}
 gpu_index_counter = 0
-
+node_name_to_corr_id_cpu = {} ##This maps node ids to correlation_ids
+node_name_to_corr_id_sm = {} ##This maps node ids to correlation_ids
+node_name_to_corr_id_ce = {} ##This maps node ids to correlation_ids
+first_sync_loc = -1
+second_sync_loc = -1 
+last_sync_loc = -1
+adding_order = [[]]
+times_cpu = {}
+times_gpu = {}
 
 def api_start_loc(_dict_api, start):
 
@@ -134,11 +142,13 @@ def find_period(_dict_api, first_match, _dict_gpu):
 
 
     global api_index
+    
+    global first_sync_loc
+    global second_sync_loc
+    global last_sync_loc
 
     first_sync = -1
-    first_sync_loc = -1
     second_sync = -1
-    second_sync_loc = -1
     
     start_loc = api_start_loc(_dict_api, first_match)
     #print("i:", start_loc)
@@ -151,27 +161,89 @@ def find_period(_dict_api, first_match, _dict_gpu):
         if(i > start_loc and _dict_api[api_index[i]]["Name"] == "cudaStreamSynchronize" and second_sync == -1):
             second_sync = api_index[i]
             second_sync_loc = i
-        if(first_sync != -1 and second_sync != -1):
-            break
-    
-        #print(_dict_api[api_index[i]])
-
+        if(i > start_loc and _dict_api[api_index[i]]["Name"] == "cudaStreamSynchronize" and first_sync != -1 and second_sync != -1):
+            last_sync_loc = i
+        
 
     print("First_sync_loc:", first_sync_loc, "First_sync:", first_sync)
     print("Second_sync_loc:", second_sync_loc, "Second_sync:", second_sync)
+    print("Last_sync_loc:", last_sync_loc)
 
-    '''
-    for i in range(first_sync_loc + 1, second_sync_loc):
-        print("##API: ", _dict_api[api_index[i]])
-        try:
-            print("##GPU:", _dict_gpu[api_index[i]])
-        except:
-            print("API?")
-    '''
-            
-    return first_sync, first_sync_loc, second_sync, second_sync_loc
+    return first_sync, first_sync_loc, second_sync, second_sync_loc ##No need to return anymore but,..
 
 
+def find_periods_and_aggregate_data(_dict_api, first_match, _dict_gpu):
+
+    ##Don't forget to add: other cpu jobs
+    
+    global api_index
+    global node_name_to_corr_id_cpu
+    global node_name_to_corr_id_sm
+    global node_name_to_corr_id_ce
+
+    global first_sync_loc
+    global second_sync_loc
+    global last_sync_loc
+
+    global adding_order
+
+    global times_cpu
+    global times_gpu
+
+    times_cpu["CPU_0"] = [1] ##or 0 idk it's in ns so, pretty effectless
+    
+    print("adding_order:", adding_order)
+
+    no_nodes_in_period = (second_sync_loc) - (first_sync_loc)
+    print("no_nodes_in_period: ", no_nodes_in_period, "len_order", len(adding_order))
+    
+    start_loc = first_sync_loc + (no_nodes_in_period * 2 )  ##Starts from second_period
+    ##Can give a table of min, max, in report
+    pointer = 0
+    for i in range(start_loc + 1, last_sync_loc + 1):
+        added_nodes_loc = pointer % no_nodes_in_period
+        _cpu_node = _dict_api[api_index[i]]
+        _corr_id = _cpu_node["CorrID"]
+        cpu_name = adding_order[added_nodes_loc][0] ##We have a CPU node anyway
+        if(cpu_name == "CPU_1"):
+            print(_cpu_node["Name"])
+        if(cpu_name in times_cpu):
+            times_cpu[cpu_name].append(_cpu_node["Duration (ns)"])
+        else:
+            times_cpu[cpu_name] = []
+            times_cpu[cpu_name].append(_cpu_node["Duration (ns)"])
+        if(_corr_id in _dict_gpu):
+            _gpu_node = _dict_gpu[_corr_id]
+            gpu_type_name = adding_order[added_nodes_loc][1] ##We are adding maximum of 2 nodes at each iteration
+            if(gpu_type_name in times_gpu):
+                times_gpu[gpu_type_name].append(_gpu_node["Duration (ns)"])
+            else:
+                times_gpu[gpu_type_name] = []
+                times_gpu[gpu_type_name].append(_gpu_node["Duration (ns)"])
+        
+        pointer = pointer + 1
+
+    sink_name = "CPU_" + str(no_nodes_in_period + 1)
+    times_cpu[sink_name] = []
+    times_cpu["Complete_Task"] = []
+    for i in range(start_loc + 1, last_sync_loc - 1, no_nodes_in_period):
+        first_cpu_node_before = _dict_api[api_index[i - no_nodes_in_period]]
+        first_cpu_node = _dict_api[api_index[i]]
+        synch_before = _dict_api[api_index[i - 1]]
+        #print("first_cpu_node:", first_cpu_node["Name"])
+        #print("synch_before: ", synch_before["Name"])
+        first_cpu_node_before_start = first_cpu_node_before["Start (ns)"]
+        first_start = first_cpu_node["Start (ns)"]
+        before_start = synch_before["Start (ns)"]
+        before_duration = synch_before["Duration (ns)"]
+        wait_time = first_start - (before_start + before_duration)
+        task_time = first_start - first_cpu_node_before_start 
+        times_cpu[sink_name].append(wait_time)
+        times_cpu["Complete_Task"] = task_time
+    print("synch_before: ", synch_before)
+    print("Ratio:", task_time / wait_time)
+
+    
 def ret_name(_type, counter):
 
     name = ""
@@ -186,6 +258,8 @@ def ret_name(_type, counter):
 
 def generate_dag_new(_dict_api, _dict_gpu, first_sync, first_sync_loc, second_sync, second_sync_loc):
 
+
+    global adding_order
     ##Also need to go from, last synchronize to next copy, this is, other cpu work
     
     DAG = nx.DiGraph()
@@ -199,8 +273,10 @@ def generate_dag_new(_dict_api, _dict_gpu, first_sync, first_sync_loc, second_sy
     _gpu_ce_counter_prev = -1
     _gpu_name_prev = -1 ##This is a string, with contrast to others
     _corr_id_to_name = {} ##This maps a cpu node with corr_id
+
+    order_counter = 0
     DAG.add_node("CPU_0", _type="CPU", _cmin=-1, _cmax=-1, amin=-1, amax=-1, _d=-1, p=-1, _q=-1, e_name="Source") ##Adding CPU
-    for i in range(first_sync_loc + 1, second_sync_loc+1): ##When like that, - +1 to +1 - it's first mem copy to last sync_loc
+    for i in range(first_sync_loc + 1, second_sync_loc + 1): ##When like that, - +1 to +1 - it's first mem copy to last sync_loc
         _api_loc = api_index[i]
         _cpu_node =  _dict_api[_api_loc]
         _cpu_node_type = -1 ## 1 normal, 2 synchronize
@@ -225,13 +301,15 @@ def generate_dag_new(_dict_api, _dict_gpu, first_sync, first_sync_loc, second_sy
         ##Scenario 1 is always true, add CPU node and edge from previous CPU if not first node
         CPU_name = ret_name("CPU", _cpu_counter)
         DAG.add_node(CPU_name, _type="CPU", e_name=_cpu_node["Name"].replace(":", "+++")) ##Adding CPU
+        adding_order[order_counter].append(CPU_name)
+        node_name_to_corr_id_cpu[CPU_name] = [_corr_id]
         if(_cpu_counter_prev != -1):
             cpu_name_prev = ret_name("CPU", _cpu_counter_prev)
-            DAG.add_edge(cpu_name_prev, CPU_name)
+            DAG.add_edge(cpu_name_prev, CPU_name, sus_min = 0, sus_max = 0)
         _corr_id_to_name[_corr_id] = CPU_name
         ##Scenario 3, add edge from latest gpu node
         if(_cpu_node_type == 2):
-            DAG.add_edge(_gpu_name_prev, CPU_name)
+            DAG.add_edge(_gpu_name_prev, CPU_name, sus_min = 0, sus_max = 0)
         _cpu_counter_prev = _cpu_counter
         _cpu_counter = _cpu_counter + 1
 
@@ -240,33 +318,65 @@ def generate_dag_new(_dict_api, _dict_gpu, first_sync, first_sync_loc, second_sy
                 _gpu_node_type = 1
                 GPU_name = ret_name("SM", _gpu_compute_counter)
                 DAG.add_node(GPU_name, _type="SM", e_name=_gpu_node["Name"].replace(":", "+++")) ##Adding CPU
+                adding_order[order_counter].append(GPU_name)
+                node_name_to_corr_id_sm[GPU_name] = [_corr_id]
                 if(_gpu_compute_counter_prev != -1 or _gpu_ce_counter_prev != -1):
-                    DAG.add_edge(_gpu_name_prev, GPU_name)
+                    DAG.add_edge(_gpu_name_prev, GPU_name, sus_min = 0, sus_max = 0)
                 _gpu_compute_counter_prev = _gpu_compute_counter
                 _gpu_compute_counter = _gpu_compute_counter + 1
                 related_cpu_node = _corr_id_to_name[_corr_id]
-                DAG.add_edge(related_cpu_node, GPU_name) ##Every GPU type node is dispatched by CPU
+                DAG.add_edge(related_cpu_node, GPU_name, sus_min = 0, sus_max = 0) ##Every GPU type node is dispatched by CPU
                 _gpu_name_prev = GPU_name
             else: ##GPU node is memory(CE) type
                 _gpu_node_type = 2
                 CE_name= ret_name("CE", _gpu_ce_counter)
                 DAG.add_node(CE_name, _type="CE", e_name=_gpu_node["Name"].replace(":", "+++")) ##Adding CPU
+                adding_order[order_counter].append(CE_name)
+                node_name_to_corr_id_ce[CE_name] = [_corr_id]
                 if(_gpu_compute_counter_prev != -1 or _gpu_ce_counter_prev != -1):
-                    DAG.add_edge(_gpu_name_prev, CE_name)
+                    DAG.add_edge(_gpu_name_prev, CE_name, sus_min = 0, sus_max = 0)
                 _gpu_ce_counter_prev = _gpu_ce_counter
                 _gpu_ce_counter = _gpu_ce_counter + 1
                 related_cpu_node = _corr_id_to_name[_corr_id]
-                DAG.add_edge(related_cpu_node, CE_name) ##Every GPU type node is dispatched by CPU
+                DAG.add_edge(related_cpu_node, CE_name, sus_min = 0, sus_max = 0) ##Every GPU type node is dispatched by CPU
                 _gpu_name_prev = CE_name
-                 
+        order_counter = order_counter + 1
+        adding_order.append([])
+    adding_order.pop()
     sink_name = ret_name("CPU", _cpu_counter)           
     DAG.add_node(sink_name, _type="CPU", _cmin=-1, _cmax=-1, amin=-1, amax=-1, _d=-1, p=-1, _q=-1, e_name="Sink") ##Adding CPU
-    DAG.add_edge("CPU_0", "CPU_1") ##Sink to first actual node
-    DAG.add_edge(ret_name("CPU", _cpu_counter - 1), sink_name) ##last node to sink
+    DAG.add_edge("CPU_0", "CPU_1", sus_min = 0, sus_max = 0) ##Source to first actual node
+    DAG.add_edge(ret_name("CPU", _cpu_counter - 1), sink_name, sus_min = 0, sus_max = 0) ##last node to sink
     return DAG
         
 
-    
+def ret_aggregated_dag(DAG):
+    print("SA")
+    new_DAG = nx.DiGraph()
+
+    nodes = dict(DAG.nodes())
+    edges = dict(DAG.edges())
+
+        
+    print("SA")
+    print("nodes:", nodes)
+    for node in nodes:
+        this_node = nodes[node]
+        if(node.find("CPU") != -1):
+            print("node:", node, "time:", times_cpu[node] )
+            c_min = min(times_cpu[node])
+            c_max = max(times_cpu[node])
+            new_DAG.add_node(node, _cmin = c_min, _cmax = c_max, _amin = -1, _amax = -1, _d = -1, _p = -1, _q = -1, e_name = this_node["e_name"])
+        if(node.find("SM") != -1 or node.find("CE") != -1):
+            c_min = min(times_gpu[node])
+            c_max = max(times_gpu[node])
+            new_DAG.add_node(node, _cmin = c_min, _cmax = c_max, _amin = -1, _amax = -1, _d = -1, _p = -1, _q = -1, e_name = this_node["e_name"])
+
+    for edge in edges:
+        new_DAG.add_edge(edge[0], edge[1])
+
+    return new_DAG
+            
 if __name__ == "__main__":
 
 
@@ -286,16 +396,19 @@ if __name__ == "__main__":
     first_sync, first_sync_loc, second_sync, second_sync_loc = find_period(_dict_api, first_match, _dict_gpu)
     #print(api_index)
     DAG = generate_dag_new(_dict_api, _dict_gpu, first_sync, first_sync_loc, second_sync, second_sync_loc)
-    nodes = DAG.nodes(data=True)
+    find_periods_and_aggregate_data(_dict_api, first_match, _dict_gpu)
+    '''
+    nodes = DAG.nodes(data=True)    
     edges = DAG.edges(data=True)
-    #print("nodes:", nodes)
-    #print("edges:", edges)
-    #for node in nodes:
-        #print(node)
-    #for edge in edges:
-        #print(edge)
-    #write_dot(DAG, "DAG.dot")
-
+    for node in nodes:
+        print(node)
+    for edge in edges:
+        print(edge)
+    '''
+    DAG = ret_aggregated_dag(DAG)
+    write_dot(DAG, "DAG.dot")
+    
+    #print("API index:", api_index)
     
     
     
